b7c876dc3e279ceaaafd8d7f897843ca2c22685f
Maniukh Yaroslav <manyukhy@gmail.com>
2025-07-13 08:47:39 +0200

    Added images and digimon list

--- data_retrive.py
+++ data_retrive.py
@@ -1,7 +1,15 @@
 import csv
+import os
 
 
 def read_enemies_data():
     with open('data/Enemies.csv') as csvfile:
         csv_reader = csv.DictReader(csvfile, delimiter=';')
         return list(csv_reader)
+
+
+def get_digi_names():
+    digimon_names = []
+    for f in os.listdir('data/images'):
+        digimon_names.append(f.replace('.jpg', ''))
+    return digimon_names
